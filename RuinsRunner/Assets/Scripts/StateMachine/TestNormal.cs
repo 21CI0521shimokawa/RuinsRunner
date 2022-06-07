b1f97599464fe6439af049ec4802b616135fe842
Kudo Taiga <21ci0117@jec.ac.jp>
2022-06-08 00:06:15 +0900

    オブジェクト基底クラスの改良、StateMachine(StateBase)の追加

---
+++ RuinsRunner/Assets/Scripts/StateMachine/TestNormal.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//テストクラス（Normal）
+public class TestNormal : StateMachine
+{
+    override protected void StateInitialize()
+    {
+        Debug.Log("NormalInitialize");
+    }
+
+    override protected StateMachine StateUpdate()
+    {
+        //Debug.Log("NormalUpdate開始");
+        StateMachine nextState = this;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            nextState = new TestRun();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log(this.StateTimeCount);
+
+            Debug.Log("現在のState:" + this.StateName);
+            Debug.Log("一つ前のState:" + this.PreviousStateName);
+        }
+
+        //Debug.Log("NormalUpdate終了");
+        return nextState;
+    }
+
+    protected override void StateFinalize()
+    {
+        Debug.Log("NormalFinalize");
+    }
+}
