1f2e5283353ca877354b19c4baacb188e6fcd666
MSI\kazus <21ci0521@jec.ac.jp>
2022-07-12 10:54:15 +0900

    MiniGameのカメラ挙動改良

--- RuinsRunner/Assets/Scripts/MainGame/Camera/CameraMG1.cs
+++ RuinsRunner/Assets/Scripts/MainGame/Camera/CameraMG1.cs
@@ -1,60 +1,59 @@
-using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using DG.Tweening;
+using UniRx;
+using UniRx.Triggers;
+
 
 public class CameraMG1 : MonoBehaviour
 {
-    [SerializeField] float CameramoveAdjust = 2f;
-    //[SerializeField] GameObject Target;
-    [SerializeField] GameObject Camera;
-    //[SerializeField] GameObject[] NextPositions;
-    //[SerializeField] PathType PathType;
-    //[SerializeField] Ease EaseType;
-    //[SerializeField] float MoveTime;
-    //Tweener tweenerMain;
-    //Tweener tweenerSub = null;
-    private void Update()
+    [SerializeField] float MoveTime;
+    [SerializeField] float LimitSpeed;
+    [SerializeField] float AfterMoving; //Debug用
+    [SerializeField] float LimitPositionY;
+    [SerializeField] GameObject Player;
+    [SerializeField] Rigidbody PlayerRigidboby;
+    [SerializeField] Ease EaseType;
+
+
+    private void Start()
     {
-        if(Camera.transform.position.y > 11)
-        {
-            Camera.transform.position -= new Vector3(0, Time.deltaTime * CameramoveAdjust, 0);
-        }
-/*        Vector3[] path = NextPositions.Select(x => x.transform.position).ToArray();
-        Camera.transform.position = path[0];
-        tweenerMain = Camera.transform.DOMoveY(path[1].y, MoveTime)
-    .OnStart(() =>
-    {//実行開始時のコールバック
-        Camera.transform.DORotate(Vector3.right * 15f, 1f);
-    });
-
-        if (Camera.transform.position.y > Target.transform.position.y + 5)
-        {
-            tweenerMain.Pause();
-
-            if (tweenerSub == null)
+        DoMove();
+        this.UpdateAsObservable()
+            .Select(_ => Mathf.Abs(PlayerRigidboby.velocity.magnitude) >= LimitSpeed)
+            .DistinctUntilChanged()
+            .Where(y => y)
+            .Take(5)
+            .Subscribe(_ =>
             {
-                tweenerSub = transform.DOMoveY(Target.transform.position.y + 5, 0.1f)
-                    .OnComplete(() =>
-                    {
-                        tweenerMain.Play();
-                        tweenerSub.Kill();
-                        tweenerSub = null;
-                    });
-            }
-            else
-            {
-                //tweenerSub.ChangeEndValue(new Vector3(transform.position.x, Target.transform.position.y, transform.position.z));
-            }
-        }
-
-
-        if (tweenerSub == null) return;
-        if ((Camera.transform.position.y + 5 - Target.transform.position.y) * (Camera.transform.position.y + 5 - Target.transform.position.y) < 0.01f)
-        {
-            tweenerSub.Complete();
-        }*/
+                StartCoroutine(AddSpeed());
+            });
     }
 
+    void DoMove()
+    {
+        this.UpdateAsObservable()
+            .Select(_=>this.transform.position.y>=LimitPositionY)
+            .Where(y=>y)
+            .Subscribe(_ =>
+            {
+                transform.position += new Vector3(0,-2, 0) * Time.deltaTime;
+            });
+    }
+    #region コルーチン
+    private IEnumerator AddSpeed()
+    {
+        var MoveTo = Player.transform.position.y -1;
+        this.transform.DOMoveY(MoveTo, 1)
+            .OnComplete(() =>
+            {
+                AfterMoving =this.transform.position.y;
+            });
+        yield return new WaitForSeconds(3);
+        Debug.Log("加速終わり");
+        yield break;
+    }
+    #endregion
 }
