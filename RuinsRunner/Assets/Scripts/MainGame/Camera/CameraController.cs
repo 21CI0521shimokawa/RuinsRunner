2e4ee5080ab383c0f60e81e9b416b1f32b37bf83
MSI\kazus <21ci0521@jec.ac.jp>
2022-08-01 14:55:52 +0900

    カメラCSをStateBaseからの変更&提出パネルの制作元プルリク

--- RuinsRunner/Assets/Scripts/MainGame/Camera/CameraController.cs
+++ RuinsRunner/Assets/Scripts/MainGame/Camera/CameraController.cs
@@ -8,13 +8,72 @@ using DG.Tweening;
 
 public class CameraController : ObjectSuperClass
 {
-    StateMachine CameraState;
+    StateMachine MapState;
+    [Header("カメラの設定関連")]
+    [SerializeField] Ease SetEaseType;
+    [Header("Positon関連")]
+    [SerializeField] Transform DefaultCameraTransform;
+    [Header("カメラのスピード")]
+    [SerializeField] float ReturnDefaultCameraPositonCameraSpeed;
+    [SerializeField] float EnemyFromBackCameraSpeed;
+    [SerializeField] float EndGameCameraSpeed;
+    [Header("Player関連")]
+    [SerializeField] Transform PlayerTransform;
+
     void Start()
     {
-        CameraState = new StateMachine(new CameraStateDefault());
+        DefaultCameraTransform = this.transform;
+        DoDefaultCameraMove(gameObject);
+        DoEnemyFromBackMove(gameObject);
     }
-    void Update()
+
+    private void DoDefaultCameraMove(GameObject CameraObject)
     {
-        CameraState.Update(this.gameObject);
+        CameraObject.ObserveEveryValueChanged(_ => MapState)
+            .Where(_ => MapState == new StateMachine(new NewMapGeneratorState_Normal()))
+            .Subscribe(_ =>
+            {
+
+                var DoRotation = new Vector3(26.6f, 0, 0);
+                CameraObject.transform.DOMove(DefaultCameraTransform.position, ReturnDefaultCameraPositonCameraSpeed)
+                    .OnUpdate(() =>
+                    {
+                        CameraObject.transform.DORotate(DoRotation, ReturnDefaultCameraPositonCameraSpeed);
+                    })
+                    .SetEase(SetEaseType);
+            });
+    }
+    private void DoEnemyFromBackMove(GameObject CameraObject)
+    {
+        CameraObject.ObserveEveryValueChanged(_ => MapState)
+            .Where(_ => MapState == new StateMachine(new NewMapGeneratorState_EnemyAttack()))
+            .Subscribe(_ =>
+            {
+                var DoRotation = new Vector3(26.7f, 180f, 0f);
+                CameraObject.transform.DOPath
+                      (
+                      new[]
+                      {
+              new Vector3(PlayerTransform.position.x,PlayerTransform.position.y+5,PlayerTransform.position.z+8),
+                      },
+                      3f, PathType.Linear
+                      ).OnUpdate(() =>
+                      {
+                          CameraObject.transform.DORotate(DoRotation, EnemyFromBackCameraSpeed);
+                      })
+                      .OnComplete(() =>
+                      {
+                      })
+                      .SetEase(SetEaseType);
+            });
+    }
+    private void GameEndCameraMove(GameObject CameraObject)
+    {
+        //MapStateが実装出来次第随時更新します
+        /*CameraObject.ObserveEveryValueChanged(_ =>MapState)
+            .Where(_=>MapState==StateMachine(new new))*/
+        var MoveTo = new Vector3(0.0f, 1.93f, 22.0f);//強引すぎるからどうにかして今後変えたい。。
+        CameraObject.transform.DOMove(MoveTo, EndGameCameraSpeed)
+                    .SetEase(SetEaseType);
     }
 }
---
+++ RuinsRunner/Assets/Scripts/MainGame/Camera/OldState/CameraMoveGameEnd.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using DG.Tweening;
+
+public class CameraMoveGameEnd : StateBase
+{
+    CameraController CameraState;
+    Transform PlayerTransform;
+    private const float MoveTime = 3.0f;
+    public override void StateInitialize()
+    {
+        CameraState = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraController>();
+        PlayerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+    public override StateBase StateUpdate(GameObject gameObject)
+    {
+        StateBase StateBase = this;
+
+        return StateBase;
+    }
+    public override void StateFinalize()
+    {//スタブ
+    }
+    private void DoEndGameMove(GameObject CameraObject)
+    {//依頼されてたカメラの動きの関数
+        var MoveTo = new Vector3(0.0f, 1.93f, 22.0f);//強引すぎるからどうにかして今後変えたい。。
+        CameraObject.transform.DOMove(MoveTo, MoveTime)
+                    .SetEase(Ease.Linear);
+    }
+}
